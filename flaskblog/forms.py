932c87bb8d1d7e0ac0a9d9067b8a2bc52fce155a
Nayan Gowda M <mnayangowda@gmail.com>
2024-06-26 11:40:54 +0530

    Saving profile Pic

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import validators, StringField, PasswordField, SubmitField, BooleanField
 from flaskblog.models import User
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -25,3 +27,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=6)])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign Up")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[validators.DataRequired(), validators.Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[validators.DataRequired(), validators.Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png','jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise validators.ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise validators.ValidationError('That email is taken. Please choose a different one.')
